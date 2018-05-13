89456c8851b3e8cca71b06e5d4c209df85cc13c2
Ibrahim Aksoy <ib.koray@gmail.com>
2018-05-13 20:25:19 +0300

    Enemy x position update function

--- js/app.js
+++ js/app.js
@@ -20,9 +20,10 @@ var Enemy = function(x, y, speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+	// You should multiply any movement by the dt parameter
+	// which will ensure the game runs at the same speed for
+	// all computers.
+	this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
